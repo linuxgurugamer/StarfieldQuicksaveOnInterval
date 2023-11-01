e5f1551c47d063988ee77b5d44e58345bdd6cb7f
Jonathan Bayer <linuxgurugamer@gmail.com>
2023-11-01 08:42:54 -0400

    Changed new file name to begin with 100 Added following new config values: 	    int QuicksaveCount = 10, Added code to delete the oldest renamed quicksave files which exceed the QuicksaveCount Added code to rename all the renamed quicksave files to start with 1001 Added Verbose level config 	// Verbose level Def 	// 	// 0 = no output after initial settings 	// 1 = normal output 	// 2 = More verbose output, with full filenames, etc Modified Console.Writelines to not output the full path, but just the filename Added  display of all options when program starts Fixed bug which happened if program was looking at files at the moment a new save is being written, it initially gets a name ending in ".tmp"

--- Program.cs
+++ Program.cs
@@ -31,6 +31,15 @@ if (!Directory.Exists(config.SaveDirectory))
     return;
 }
 
+Console.WriteLine("\nSavedirectory: " + config.SaveDirectory);
+Console.WriteLine("UpdateInterval: " + config.UpdateInterval);
+Console.WriteLine("QuicksaveSave: " + config.QuicksaveSave);
+Console.WriteLine("QuicksaveSaveInterval: " + config.QuicksaveSaveInterval);
+Console.WriteLine("QuicksaveCopy: " + config.QuicksaveCopy);
+Console.WriteLine("QuicksaveCount: " + config.QuicksaveCount);
+Console.WriteLine("VerboseLevel: " + config.VerboseLevel + "\n");
+
+
 CancellationTokenSource cancel = new();
 Console.CancelKeyPress += (_, e) =>
 {
@@ -60,23 +69,32 @@ while (!cancel.Token.IsCancellationRequested)
     GetWindowThreadProcessId(GetForegroundWindow(), out uint processId);
     Process process = Process.GetProcessById((int)processId);
 
+    bool linesWritten = false;
     if (process.MainWindowTitle != config.ProcessName)
     {
-        Console.WriteLine($"Skipping this update because {config.ProcessName} was not in focus");
+        if (config.VerboseLevel > 0)
+        {
+            Console.WriteLine($"Skipping this update because {config.ProcessName} was not in focus");
+            linesWritten = true;
+        }
         continue;
     }
 
     IEnumerable<string> saveFiles = Directory.EnumerateFiles(config.SaveDirectory);
     IEnumerable<(string, DateTime)> quicksaveFileCandidates = saveFiles
-        .Where(x => Path.GetFileName(x).StartsWith("Quicksave0"))
+        .Where(x => Path.GetFileName(x).StartsWith("Quicksave0") && Path.GetFileName(x).EndsWith(".sfs") )
         .Select(x => (x, File.GetLastWriteTime(x)))
         .OrderByDescending(x => x.Item2);
 
     int quicksaveFileCandidatesCount = quicksaveFileCandidates.Count();
     if (quicksaveFileCandidatesCount == 0)
     {
-        Console.WriteLine($"Skipping this update because no quicksaves were found in '{config.SaveDirectory}'");
-        continue;
+        if (config.VerboseLevel > 0)
+        {
+            Console.WriteLine($"Skipping this update because no quicksaves were found in '{config.SaveDirectory}'");
+            linesWritten = true;
+        }
+            continue;
     }
 
     (string quicksaveFilePath, DateTime quicksaveFileWriteTime) = quicksaveFileCandidates.First();
@@ -84,11 +102,15 @@ while (!cancel.Token.IsCancellationRequested)
 
     if (quicksaveFileCandidatesCount > 1)
     {
-        Console.WriteLine($"Found more than one quicksave file in '{config.SaveDirectory}'. " +
+        if (config.VerboseLevel > 0)
+        {
+            Console.WriteLine($"Found more than one quicksave file in '{config.SaveDirectory}'. " +
             $"Selected '{Path.GetFileName(quicksaveFilePath)}' as it was most recently modified. " +
             $"Candidates were:");
+            linesWritten = true;
+        }
 
-        string candidates = string.Join("\n  ", quicksaveFileCandidates.Select(x => $"'{Path.GetFileName(x.Item1)}'"));
+            string candidates = string.Join("\n  ", quicksaveFileCandidates.Select(x => $"'{Path.GetFileName(x.Item1)}'"));
         Console.WriteLine($"  {candidates}");
     }
 
@@ -104,12 +126,19 @@ while (!cancel.Token.IsCancellationRequested)
             .DefaultIfEmpty(0)
             .Max();
 
-        string savePath = quicksaveFilePath.Replace("Quicksave0", $"Save{highestSaveId + 1}");
+        string savePath;
+        if (highestSaveId < 100)
+            savePath = quicksaveFilePath.Replace("Quicksave0", $"Save100{highestSaveId + 1}");
+        else
+            savePath = quicksaveFilePath.Replace("Quicksave0", $"Save{highestSaveId + 1}");
 
-        Console.WriteLine(
-            $"Copying '{quicksaveFilePath}' to '{savePath}' because quicksave was " +
-            $"modified {timeSinceLastQuicksave} ago (at {quicksaveFileWriteTime})");
-
+        if (config.VerboseLevel > 0)
+        {
+            Console.WriteLine(
+                $"Copying '{quicksaveFilePath.Substring(1 + quicksaveFilePath.LastIndexOf(Path.DirectorySeparatorChar))}' to '{savePath.Substring(1 + savePath.LastIndexOf(Path.DirectorySeparatorChar))}' because quicksave was " +
+                $"modified {timeSinceLastQuicksave} ago (at {quicksaveFileWriteTime})");
+            linesWritten = true;
+        }
         if (TryCopyFile(quicksaveFilePath, savePath))
         {
             quicksaveLastCopy = quicksaveFileWriteTime;
@@ -119,12 +148,93 @@ while (!cancel.Token.IsCancellationRequested)
     // Handles saving (detect when it's been an interval after our most recent quicksave, make one)
     if (config.QuicksaveSave && timeSinceLastQuicksave >= TimeSpan.FromSeconds(config.QuicksaveSaveInterval))
     {
-        Console.WriteLine(
+        if (config.VerboseLevel > 0)
+        {
+            Console.WriteLine(
             $"Sending F5 to {config.ProcessName} because quicksave was " +
             $"modified {timeSinceLastQuicksave} ago (at {quicksaveFileWriteTime})");
-
-        inputSimulator.Keyboard.KeyDown(VirtualKeyCode.F5).Sleep(200).KeyUp(VirtualKeyCode.F5);
+            linesWritten = true;
+        }
+            inputSimulator.Keyboard.KeyDown(VirtualKeyCode.F5).Sleep(200).KeyUp(VirtualKeyCode.F5);
     }
+
+    ///////////////////////////////////////////////////////////////////
+    // Now delete the oldest, keeping the QuicksaveCount number of files
+
+    IEnumerable<string> oldSaveFiles = Directory.EnumerateFiles(config.SaveDirectory);
+
+    IEnumerable<(string, DateTime)> quicksaveDelFileCandidates = saveFiles
+       .Where(x => Path.GetFileName(x).StartsWith("Save1") && Path.GetFileName(x).EndsWith(".sfs") && Path.GetFileName(x)[8] == '_')
+       .Select(x => (x, File.GetLastWriteTime(x)))
+       .OrderBy(x => x.Item2);
+
+    if (config.VerboseLevel > 3)
+    {
+        foreach (var f in quicksaveDelFileCandidates)
+        {
+            Console.WriteLine("quickSaveFile: " + f.Item1 + "   DateTime: " + f.Item2.ToShortTimeString());
+        }
+        Console.WriteLine(" ");
+        linesWritten = true;
+    }
+    int numFiles = quicksaveDelFileCandidates.Count();
+    if (numFiles <= config.QuicksaveCount && config.VerboseLevel > 1)
+    {
+        Console.WriteLine("No old save files to be deleted");
+        linesWritten = true;
+    }
+    else
+    {
+        numFiles -= config.QuicksaveCount;
+        if (quicksaveDelFileCandidates.Count() > 0)
+        {
+            string lastFile = quicksaveDelFileCandidates.Last().Item1;
+            var i = lastFile.Substring(7);
+            var i1 = i.Substring(0, i.IndexOf('_'));
+
+            foreach (var f in quicksaveDelFileCandidates)
+            {
+                if (--numFiles <= 0)
+                    break;
+
+                if (config.VerboseLevel > 0)
+                {
+                    Console.WriteLine("Deleting old file: " + f.Item1.Substring(1 + f.Item1.LastIndexOf(Path.DirectorySeparatorChar)));
+                    linesWritten = true;
+                }
+                    System.IO.File.Delete(f.Item1);
+
+            }
+            // Now need to rename existing files
+            saveFiles = Directory.EnumerateFiles(config.SaveDirectory);
+
+            IEnumerable<(string, DateTime)> renamedSaveFiles = saveFiles
+               .Where(x => Path.GetFileName(x).StartsWith("Save1") && Path.GetFileName(x).EndsWith(".sfs") && Path.GetFileName(x)[8] == '_')
+               .Select(x => (x, File.GetLastWriteTime(x)))
+               .OrderBy(x => x.Item2);
+
+            int startSaveId = 1001;
+            foreach (var f in renamedSaveFiles)
+            {
+                int iStart = f.Item1.IndexOf("Save1");
+                string saveFileName = f.Item1.Substring(iStart, 8);
+
+                var newSavePath = f.Item1.Replace(saveFileName, $"Save{startSaveId}");
+                if (config.VerboseLevel > 1)
+                {
+                    Console.WriteLine("Old filename: " + saveFileName + ", newSavePath: " + newSavePath + ", startSaveId: " + startSaveId);
+                    linesWritten = true;
+                }
+                    if (f.Item1 != newSavePath)
+                    System.IO.File.Move(f.Item1, newSavePath);
+                startSaveId++;
+
+            }
+        }
+    }
+    if (linesWritten )
+        Console.WriteLine(" ");
+
 }
 
 bool TryCopyFile(string source, string dest)
@@ -144,10 +254,19 @@ bool TryCopyFile(string source, string dest)
     return true;
 }
 
+// Verbose level Def
+//
+// 0 = no output after initial settings
+// 1 = normal output
+// 2 = More verbose output, with full filenames, etc
+
 record Config(
     string SaveDirectory,
     string ProcessName = "Starfield",
     float UpdateInterval = 10.0f,
     bool QuicksaveSave = true,
     float QuicksaveSaveInterval = 120.0f,
-    bool QuicksaveCopy = true);
+    bool QuicksaveCopy = true,
+    int QuicksaveCount = 10,
+    int VerboseLevel = 1
+    );
